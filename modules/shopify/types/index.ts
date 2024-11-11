3dd59f95248b0be6bd565222dbad8e5b4dde38ff
Baroshem <jakub.andrzejewski.dev@gmail.com>
2024-11-11 19:54:56 +0100

    fix: create local enum export

--- modules/shopify/types/index.ts
+++ modules/shopify/types/index.ts
@@ -15,4 +15,28 @@ export type ShopifyProducts = GetProductsQuery['products']
 export type ShopifyCollection = CollectionFragment
 export type ShopifyCollections = GetCollectionsQuery['collections']
 export type ShopifyCollectionSortKey = GetCollectionQueryVariables['sortKey']
-export { ProductCollectionSortKeys as ShopifyCollectionSortKeys } from '#gql/default'
+
+// Following export of enum breaks CI
+// export { ProductCollectionSortKeys as ShopifyCollectionSortKeys } from '#gql/default'
+export enum ShopifyCollectionSortKeys {
+  /** Sort by the `best-selling` value. */
+  BEST_SELLING = 'BEST_SELLING',
+  /** Sort by the `collection-default` value. */
+  COLLECTION_DEFAULT = 'COLLECTION_DEFAULT',
+  /** Sort by the `created` value. */
+  CREATED = 'CREATED',
+  /** Sort by the `id` value. */
+  ID = 'ID',
+  /** Sort by the `manual` value. */
+  MANUAL = 'MANUAL',
+  /** Sort by the `price` value. */
+  PRICE = 'PRICE',
+  /**
+   * Sort by relevance to the search terms when the `query` parameter is specified on the connection.
+   * Don't use this sort key when no search query is specified.
+   *
+   */
+  RELEVANCE = 'RELEVANCE',
+  /** Sort by the `title` value. */
+  TITLE = 'TITLE',
+}
