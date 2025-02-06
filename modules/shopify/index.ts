8b7410dd4fc590f071499b07375044173fa92479
Baroshem <jakub.andrzejewski.dev@gmail.com>
2025-02-06 14:35:07 +0100

    feat: rewrite env variables for module

--- modules/shopify/index.ts
+++ modules/shopify/index.ts
@@ -5,6 +5,9 @@ import {
   addImportsDir,
 } from '@nuxt/kit'
 
+const DEFAULT_SHOPIFY_GRAPHQL_API_URL = 'https://graphql.myshopify.com/api/2024-07/graphql.json'
+const DEFAULT_SHOPIFY_ACCESS_TOKEN = 'ecdc7f91ed0970e733268535c828fbbe'
+
 export default defineNuxtModule({
   meta: {
     name: 'shopify',
@@ -16,23 +19,23 @@ export default defineNuxtModule({
     addImportsDir(resolve('types'))
 
     if (!process.env.SHOPIFY_GRAPHQL_API_URL) {
-      throw Error(`Required Environment Variable 'SHOPIFY_GRAPHQL_API_URL' missing`)
+      console.warn(`[nuxt-commerce] Environment Variable 'SHOPIFY_GRAPHQL_API_URL' missing. Using default value '${DEFAULT_SHOPIFY_GRAPHQL_API_URL}'`)
     }
 
     if (!process.env.SHOPIFY_ACCESS_TOKEN) {
-      throw Error(`Required Environment Variable 'SHOPIFY_ACCESS_TOKEN' missing`)
+      console.warn(`[nuxt-commerce] Environment Variable 'SHOPIFY_ACCESS_TOKEN' missing. Using default value '${DEFAULT_SHOPIFY_ACCESS_TOKEN}'`)
     }
 
     await installModule('nuxt-graphql-client', {
       documentPaths: ['../modules/shopify/graphql'],
       clients: {
         default: {
-          host: process.env.SHOPIFY_GRAPHQL_API_URL as string,
+          host: process.env.SHOPIFY_GRAPHQL_API_URL as string || DEFAULT_SHOPIFY_GRAPHQL_API_URL,
           codegenHeaders: {
-            'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN as string,
+            'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN as string || DEFAULT_SHOPIFY_ACCESS_TOKEN,
           },
           headers: {
-            'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN as string,
+            'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN as string || DEFAULT_SHOPIFY_ACCESS_TOKEN,
             'Content-Type': 'application/json',
           },
         },
