d5d11966660458ff9f518b6a9e28a986a6ad2eee
Baroshem <jakub.andrzejewski.dev@gmail.com>
2025-02-09 07:40:32 +0100

    feat: improvements to the error page

--- app/error.vue
+++ app/error.vue
@@ -1,33 +1,28 @@
 <script setup lang="ts">
-defineProps({
-  error: {
-    type: Object,
-    default: () => ({}),
-  },
-})
+import type { NuxtError } from '#app'
 
-const handleError = () => clearError({ redirect: '/' })
+defineProps<{ error: NuxtError }>()
 </script>
 
 <template>
-  <Body class="font-body bg-slate-950" />
-  <main class="max-w-[1536px] text-center mx-auto my-0">
+  <Body class="bg-[#020420]" />
+  <main class="text-center mx-auto my-0">
     <LayoutNavbarTop class="mb-5" />
     <UNotifications />
     <section>
-      <h2 class="text-9xl mb-4 text-slate-400">
+      <h2 class="text-9xl mb-4">
         {{ error.statusCode }}
       </h2>
-      <h3 class="text-3xl text-slate-400">
+      <h3 class="text-3xl">
         {{ error.message }}
       </h3>
       <UButton
         class="mt-4"
-        @click="handleError"
+        @click="clearError({ redirect: '/' })"
       >
         Back to home
       </UButton>
     </section>
-    <LayoutLazyFooter class="mt-5" />
+    <LazyLayoutTheFooter class="mt-5" />
   </main>
 </template>
